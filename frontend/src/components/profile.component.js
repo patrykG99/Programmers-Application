e6d2932dc0eaec120f299271f635d20ed8db5c63
qxeer <qxeer84@gmail.com>
2023-07-27 01:27:30 +0200

    Added project table from Material UI and pagination.

--- frontend/src/components/profile.component.js
+++ frontend/src/components/profile.component.js
@@ -5,11 +5,105 @@ import Row from 'react-bootstrap/Row';
 import {useParams, useNavigate} from 'react-router-dom';
 import { useState, useEffect } from "react";
 import { Button } from "bootstrap";
-import Table from 'react-bootstrap/Table';
+//import Table from 'react-bootstrap/Table';
 import { Rating } from 'react-simple-star-rating'
 import "./styles.scss"
 import {Modal} from "react-bootstrap";
 import {Container ,ListGroup, Col} from 'react-bootstrap';
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Paper,
+    IconButton,
+    createTheme
+} from "@mui/material";
+import FirstPageIcon from '@mui/icons-material/FirstPage';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import LastPageIcon from '@mui/icons-material/LastPage';
+import Box from '@mui/material/Box';
+import { useTheme } from '@mui/material/styles';
+import TableFooter from '@mui/material/TableFooter';
+import TablePagination from '@mui/material/TablePagination';
+
+
+interface TablePaginationActionsProps {
+    count: number;
+    page: number;
+    rowsPerPage: number;
+    onPageChange: (
+        event: React.MouseEvent<HTMLButtonElement>,
+        newPage: number,
+    ) => void;
+}
+
+function TablePaginationActions(props: TablePaginationActionsProps) {
+    const theme = createTheme({
+        palette: {
+            primary: {
+                main: "#F2AA4CFF",
+            },
+        },
+    });
+    const { count, page, rowsPerPage, onPageChange } = props;
+
+    const handleFirstPageButtonClick = (
+        event: React.MouseEvent<HTMLButtonElement>,
+    ) => {
+        onPageChange(event, 0);
+    };
+
+    const handleBackButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        onPageChange(event, page - 1);
+    };
+
+    const handleNextButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        onPageChange(event, page + 1);
+    };
+
+    const handleLastPageButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+    };
+
+    return (
+        <Box sx={{ flexShrink: 0, ml: 2.5 }}>
+            <IconButton
+                onClick={handleFirstPageButtonClick}
+                disabled={page === 0}
+                aria-label="first page"
+            >
+                {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
+
+            </IconButton>
+            <IconButton
+                onClick={handleBackButtonClick}
+                disabled={page === 0}
+                aria-label="previous page"
+            >
+                {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+            </IconButton>
+            <IconButton
+                onClick={handleNextButtonClick}
+                disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+                aria-label="next page"
+            >
+                {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+            </IconButton>
+            <IconButton
+                onClick={handleLastPageButtonClick}
+                disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+                aria-label="last page"
+            >
+                {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
+            </IconButton>
+        </Box>
+    );
+}
+
 
 
 export default function ProjectPage(props) {
@@ -22,11 +116,28 @@ export default function ProjectPage(props) {
   const [userReviews, setUserReviews] = useState([])
   const [changeDesc, setChangeDesc] = useState(false)
   const [newDesc, setNewDesc] = useState('')
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(5);
+    const emptyRows =
+        page > 0 ? Math.max(0, (1 + page) * rowsPerPage - userProjects.length) : 0;
 
+    const handleChangePage = (
+        event: React.MouseEvent<HTMLButtonElement> | null,
+        newPage: number,
+    ) => {
+        setPage(newPage);
+    };
+    const handleChangeRowsPerPage = (
+        event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    ) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
   
  
 
   const navigate = useNavigate();
+
 
   let { id } = useParams();
   const handleNameChange = event => {
@@ -295,25 +406,73 @@ export default function ProjectPage(props) {
               <div className="profileBorder" style={{height:'50%', width:'60%', float:'left'}}>
                   <div className="sectionTitle"  style={{width:'100%', height:'10%', padding:'2px', color:'white'}}><h4>User Information</h4><hr/>
 
-                              <ListGroup style={{width:'100%',backgroundColor:'#F2AA4CFF'}} className="custom-list-group">
-                                  <ListGroup.Item bsStyle="success" className="listHeaders">
-                                      <div>
-                                          <div className="userProjectsItem">Project name</div>
-                                          <div className="userProjectsItem">Project owner</div>
-                                          <div className="userProjectsItem">Programming language</div>
-                                      </div>
-                                  </ListGroup.Item>
-                                  {userProjects.map(userProject=>
-                                  <ListGroup.Item>
-                                      <div className="userProjectsItem">{userProject.name}</div>
-                                      <div className="userProjectsItem">{userProject.tech}</div>
-                                      <div className="userProjectsItem">{userProject.tech}</div>
+                              {/*<ListGroup style={{width:'100%',backgroundColor:'#F2AA4CFF'}} className="custom-list-group projectList">*/}
+                              {/*    <ListGroup.Item bsStyle="success" className="listHeaders">*/}
+                              {/*        <div>*/}
+                              {/*            <div className="userProjectsItem">Project name</div>*/}
+                              {/*            <div className="userProjectsItem">Project owner</div>*/}
+                              {/*            <div className="userProjectsItem">Programming language</div>*/}
+                              {/*        </div>*/}
+                              {/*    </ListGroup.Item>*/}
 
-                                  </ListGroup.Item>
-                                  )}
-                              </ListGroup>
+                              {/*    {userProjects.map(userProject=>*/}
+                              {/*    <ListGroup.Item>*/}
+                              {/*        <div className="userProjectsItem">{userProject.name}</div>*/}
+                              {/*        <div className="userProjectsItem">{userProject.tech}</div>*/}
+                              {/*        <div className="userProjectsItem">{userProject.tech}</div>*/}
+
+                              {/*    </ListGroup.Item>*/}
+                              {/*    )}*/}
+                              {/*</ListGroup>*/}
 
 
+
+
+                  </div>
+                  <div className="tableContainer">
+                      <Table stickyHeader >
+                          <TableHead sx={{bgcolor:'#F2AA4CFF'}}>
+                              <TableRow>
+                                  <TableCell>Project Name</TableCell>
+                                  <TableCell>Used Language</TableCell>
+                                  <TableCell>Used Language</TableCell>
+                              </TableRow>
+                          </TableHead>
+                          <TableBody>
+                              {(rowsPerPage > 0
+                                      ? userProjects.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                                      : userProjects
+                              ).map((row) => (
+                                  <TableRow key={row.id}>
+                                      <TableCell component="th" scope="row">
+                                          {row.name}
+                                      </TableCell>
+                                      <TableCell>{row.tech}</TableCell>
+                                      <TableCell>{row.description}</TableCell>
+                                  </TableRow>
+                              ))}
+                          </TableBody>
+                          <TableFooter>
+                              <TableRow>
+                                  <TablePagination
+                                      rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+                                      colSpan={3}
+                                      count={userProjects.length}
+                                      rowsPerPage={rowsPerPage}
+                                      page={page}
+                                      SelectProps={{
+                                          inputProps: {
+                                              'aria-label': 'rows per page',
+                                          },
+                                          native: true,
+                                      }}
+                                      onPageChange={handleChangePage}
+                                      onRowsPerPageChange={handleChangeRowsPerPage}
+                                      ActionsComponent={TablePaginationActions}
+                                  />
+                              </TableRow>
+                          </TableFooter>
+                      </Table>
                   </div>
               </div>
               <div className="profileBorder" style={{height:'50%', width:'20%',float:'right'}}>
