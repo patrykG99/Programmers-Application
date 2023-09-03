b4c325daba6dc9ff36df63a3b9b7fbf78550bd2d
qxeer <qxeer84@gmail.com>
2023-09-03 18:02:45 +0200

    Started rebuilding projects list, added search by name

--- frontend/src/components/pages.component.js
+++ frontend/src/components/pages.component.js
@@ -17,93 +17,80 @@ export default function Pages(props) {
   const [projects, setProjects] = useState();
   const [pageNumber, setPageNumber] = useState(0);
   const [hasLoaded, setHasLoaded] = useState(false)
-  const displayProjects = 
-  useMemo(() => {
 
-    async function getData() {
+    const [searchTerm, setSearchTerm] = useState('');
 
-        const user = JSON.parse(localStorage.getItem('user'));
-        console.log(user.accessToken);
-        const response = await fetch('http://localhost:8080/api/projects',{
-          method:'GET', headers:{"Authorization":'Bearer ' +user.accessToken}
-        });
-        const body = await response.json();
-        
-        setProjects(body);
-        setHasLoaded(true)
+    useEffect(() => {
+        async function fetchData() {
+            const user = JSON.parse(localStorage.getItem('user'));
+            console.log(user.accessToken);
+            const response = await fetch('http://localhost:8080/api/projects',{
+                method:'GET', headers:{"Authorization":'Bearer ' +user.accessToken}
+            });
+            const body = await response.json();
 
-    }
+            setProjects(body);
+            setHasLoaded(true)
+        }
+        fetchData();
+    }, []);
+
+  if(hasLoaded) {
+      const filteredProjects = projects.filter(project =>
+          project.name.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      const projectsPerPage = 6;
+      const pageCount = Math.ceil(filteredProjects.length / projectsPerPage);
+      const pagesVisited = pageNumber * projectsPerPage;
+
+      const displayedProjects = filteredProjects
+          .slice(pagesVisited, pagesVisited + projectsPerPage)
+          .map(project => (
+              <li key={project.id}>
+                  {project.name} - {project.tech}
+              </li>
+          ));
+      const changePage = ({ selected }) => {
+          setPageNumber(selected);
+      };
 
 
-    getData()
-  }, []);
-  
-  if(hasLoaded){
-  const projectsPerPage = 6
-  const pageCount = Math.ceil(projects.length / projectsPerPage)
-  const pagesVisited = pageNumber * projectsPerPage
 
-    const changePage = ({selected}) =>{
-        setPageNumber(selected)
-    }
+
+      console.log(projects)
 
 
 
 
+      return (
+          <div>
+              <h2>Projects</h2>
+              <input
+                  type="text"
+                  placeholder="Search projects"
+                  onChange={(e) => setSearchTerm(e.target.value)}
+              />
+              <Link to="/projectAdd">
+                  <Button variant="primary">Create New Project</Button>
+              </Link>
+              <ul>
+                  {displayedProjects}
+              </ul>
+              <ReactPaginate
+                  previousLabel={"Previous"}
+                  nextLabel={"Next"}
+                  pageCount={pageCount}
+                  onPageChange={changePage}
+                  containerClassName={"paginationBttns"}
+                  previousLinkClassName={"previousBttn"}
+                  nextLinkClassName={"nextBttn"}
+                  disabledClassName={"paginationDisabled"}
+                  activeClassName={"paginationActive"}
+              />
+          </div>
+      );
 
-  console.log(projects)
-  
-    const displayProjects = projects.slice(pagesVisited, pagesVisited + projectsPerPage).map(project => {
-        return(<>
-            
-            <div>
-              
-                    
-                    
-                    
-                    
-                    <Card id="projectCard" style={{width:'22rem' }}>
-                      <Card.Header style={{backgroundColor:'#101820FF', color:'white'}}>{project.tech}</Card.Header>
-                      <Card.Body>
-                      <Card.Title><div key={project.id}>
-                          {project.name} 
-                        </div></Card.Title>
-                        <Card.Text>
-                          {project.description}<br/><br/>
-                          <a href={"/projects/" + project.id} class="btn btn-primary stretched-link">Go to project</a>
-                        </Card.Text>
-                        </Card.Body>
-                        </Card>
-                        
-                    
-                    
-                    
-                  
-              
-              </div>
-              
-              </>);
-    
-  })
-  
-
-  return <div className='App'><h2>Projects</h2><div style={{disply:'flex', justifyContent:'left'}}>
-              <Link to={"/projectAdd"} className="nav-link">
-              <Button variant="primary" style={{float:'right'}}>Create New Project</Button>{' '}
-              </Link></div><Row xs={3} md={3} lg={3} className="g-7">{displayProjects}</Row>
-  <ReactPaginate
-    previousLabel={"Previous"}
-    nextLabel={"Next"}
-    pageCount={pageCount}
-    onPageChange={changePage}
-    containerClassName={"paginationBttns"}
-    previousLinkClassName={"previousBttn"}
-    nextLinkClassName={"nextBttn"}
-    disabledClassName={"paginationDisabled"}
-    activeClassName={"paginationActive"}
-  /></div>}
-  
-  
+  }
 
   
 }
